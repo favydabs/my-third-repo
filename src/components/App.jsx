950a7f6e3620d1c0ac7f3c0aa8a3ead57a7b055f
favydabs <favourdabit@gmail.com>
2022-06-19 14:31:34 +0100

    fifth commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
-function createCard(contact) {
-  return (
-    <Card
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.tel}
-      email={contact.email}
-    />
-  );
-}
+
+
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
+      {contacts.map(contact => (
+        <Card
+          key={contact.id}
+          name={contact.name}
+          img={contact.imgURL}
+          tel={contact.tel}
+          email={contact.email}
+        />
+      ))}
       {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
